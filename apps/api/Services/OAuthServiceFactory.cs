f7c2ac026c45328d4b83e681c8986b70be0fb7db
Drinix <thedrinix@gmail.com>
2025-08-26 19:54:00 +0200

    [Api] Add Google OAuth service implementation and factory integration

---
+++ apps/api/Services/OAuthServiceFactory.cs
@@ -0,0 +1,15 @@
+﻿using SnowrunnerMerger.Api.Services.Interfaces;
+
+namespace SnowrunnerMerger.Api.Services;
+
+public class OAuthServiceFactory(IServiceProvider provider) : IOAuthServiceFactory
+{
+    public OAuthService GetService(string providerName)
+    {
+        return providerName.ToLower() switch
+        {
+            "google" => provider.GetRequiredService<OAuthProviders.GoogleOAuthService>(),
+            _ => throw new NotSupportedException($"OAuth provider '{providerName}' is not supported.")
+        };
+    }
+}
