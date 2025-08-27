d3a394fa4bce0e9f4f11c9eea9775d7a007e0551
Drinix <thedrinix@gmail.com>
2025-08-27 11:50:59 +0200

    [Api] Refactor JWT Bearer authentication events to simplify token validation logic

--- apps/api/Services/Interfaces/ITokenService.cs
+++ apps/api/Services/Interfaces/ITokenService.cs
@@ -5,7 +5,7 @@ namespace SnowrunnerMerger.Api.Services.Interfaces;
 
 public interface ITokenService
 {
-    public const int AccessTokenLifetime = 60 * 60 * 3; // 3 hours in seconds
+    public const int AccessTokenLifetime = 60 * 30; // 30 minutes in seconds
     public const int RefreshTokenLifetime = 60 * 60 * 24 * 30; // 30 days in seconds
 
     Task<AccountConfirmationToken> GenerateConfirmationToken(User user);
@@ -14,5 +14,5 @@ public interface ITokenService
     Task<AccountLinkingToken> GenerateLinkingToken(User user, string oAuthProvider, string providerAccountId);
     Task<RefreshTokenData> GenerateRefreshToken(User user, bool extendedLifespan = false, bool storeInCookie = true);
     Task<RefreshTokenData?> ValidateRefreshToken(string token);
-    string GenerateJwt(JwtData data);
+    string GenerateJwt(Guid userId, string role = "User");
 }
